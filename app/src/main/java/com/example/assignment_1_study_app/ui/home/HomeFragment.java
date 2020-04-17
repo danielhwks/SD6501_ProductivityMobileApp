885e639d3d9e7334e28e3007d6b92376a143f545
GoogzGazuli <danielhwks9@gmail.com>
2020-04-17 13:41:51 +1200

    Added shortcuts to home fragment

--- app/src/main/java/com/example/assignment_1_study_app/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/assignment_1_study_app/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.assignment_1_study_app.R;
 
@@ -20,6 +23,25 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
+        final Button btnShortcutTimer = root.findViewById(R.id.btnShortcutTimer);
+        final Button btnShortcutNotes = root.findViewById(R.id.btnShortcutNotes);
+
+        textView.setText("What would you like to do?");
+
+        btnShortcutTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController controller = Navigation.findNavController(v);
+                controller.navigate(R.id.nav_timer);
+            }
+        });
+        btnShortcutNotes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController controller = Navigation.findNavController(v);
+                controller.navigate(R.id.nav_notes);
+            }
+        });
 
         return root;
     }
